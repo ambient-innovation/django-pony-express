bb1e4132a507af7b1f5b22f3fb55a80c9444dd37
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2026-07-03 15:30:15 +0200

    Use DEPLOYMENT_STATUS_STABLE constant for development_status

--- .ambient-package-update/metadata.py
+++ .ambient-package-update/metadata.py
@@ -1,5 +1,6 @@
 from ambient_package_update.metadata.author import PackageAuthor
 from ambient_package_update.metadata.constants import (
+    DEPLOYMENT_STATUS_STABLE,
     DEV_DEPENDENCIES,
     LICENSE_MIT,
     SUPPORTED_DJANGO_VERSIONS,
@@ -23,7 +24,7 @@ METADATA = PackageMetadata(
     licenser="Beyonder Deutschland GmbH",
     license=LICENSE_MIT,
     license_year=2023,
-    development_status="5 - Production/Stable",
+    development_status=DEPLOYMENT_STATUS_STABLE,
     has_migrations=False,
     claim="Class-based emails including a test suite for Django",
     readme_content=ReadmeContent(uses_internationalisation=True),
